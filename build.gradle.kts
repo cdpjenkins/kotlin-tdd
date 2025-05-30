46a5effafc4ea07aa1ca071d1b066e72f1b61f28
Chris Jenkins <cdpjenkins@gmail.com>
2025-05-30 20:51:04 +0100

    Parameterise the tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
 tasks.test {
--- src/test/kotlin/HelloKotlinTest.kt
+++ src/test/kotlin/HelloKotlinTest.kt
@@ -1,52 +1,30 @@
 import org.example.fizzBuzz
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
 
 class HelloKotlinTest {
 
-    @Test
-    fun `passing 1 as parameter returns 1`() {
-        assertEquals("1", fizzBuzz(1))
+    @ParameterizedTest
+    @CsvSource(
+        "1,1",
+        "2,2",
+        "3,fizz",
+        "6,fizz",
+        "9,fizz",
+        "5,buzz",
+        "10,buzz",
+        "15,fizzbuzz",
+        "30,fizzbuzz")
+    fun `fizzbuzz returns correct result`(input: Int, expected: String) {
+        assertEquals(expected, fizzBuzz(input))
     }
 
-    @Test
-    fun `passing 2 returns 2`() {
-        assertEquals("2", fizzBuzz(2))
-    }
 
     @Test
-    fun `passing 3 returns fizz`() {
-        assertEquals("fizz", fizzBuzz(3))
-    }
-
-    @Test
-    fun `passing 5 returns buzz`() {
-        assertEquals("buzz", fizzBuzz(5))
-    }
-
-    @Test
-    fun `passing 10 returns buzz`() {
-        assertEquals("buzz", fizzBuzz(10))
-    }
-
-    @Test
-    fun `passing 6 returns fizz`() {
-        assertEquals("fizz", fizzBuzz(6))
-    }
-
-    @Test
-    fun `passing 9 returns fizz`() {
-        assertEquals("fizz", fizzBuzz(9))
-    }
-
-    @Test
-    fun `passing -1 throws IllegalArgumentException`() {
+    fun `passing negative number throws IllegalArgumentException`() {
         assertThrows<IllegalArgumentException> { fizzBuzz(-1) }
-    }
-
-    @Test
-    fun `passing 15 returns fizzbuzz`() {
-        assertEquals("fizzbuzz", fizzBuzz(15))
     }
 }
